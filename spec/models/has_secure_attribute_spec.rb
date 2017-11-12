d7c86deec0d057d192e98c6b161dc72f4dff6242
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-11-12 08:45:29 +0200

    Rspec using expect instead of should

--- spec/models/has_secure_attribute_spec.rb
+++ spec/models/has_secure_attribute_spec.rb
@@ -1,64 +1,64 @@
 require 'spec_helper'
 
 describe TestModelWithAttribute do
-  it { should respond_to(:security_answer)               }
-  it { should respond_to(:security_answer=)              }
-  it { should respond_to(:security_answer_confirmation)  }
-  it { should respond_to(:security_answer_confirmation=) }
-  it { should respond_to(:authenticate_security_answer)  }
+  it { expect(subject).to respond_to(:security_answer)               }
+  it { expect(subject).to respond_to(:security_answer=)              }
+  it { expect(subject).to respond_to(:security_answer_confirmation)  }
+  it { expect(subject).to respond_to(:security_answer_confirmation=) }
+  it { expect(subject).to respond_to(:authenticate_security_answer)  }
 
   it 'should confirm security answer' do
     subject.security_answer = 'hello there'
     subject.security_answer_confirmation = 'there hello'
     subject.valid?
-    subject.errors[:security_answer_confirmation].should include("doesn't match Security answer")
+    expect(subject.errors[:security_answer_confirmation]).to include("doesn't match Security answer")
   end
 
   it 'should not confirm security answer if not given' do
     subject.security_answer = nil
     subject.security_answer_confirmation = 'there hello'
     subject.valid?
-    subject.errors[:security_answer_confirmation].should be_blank
+    expect(subject.errors[:security_answer_confirmation]).to be_blank
   end
 
   it 'should require security answer on create' do
-    subject.should be_new_record
+    expect(subject).to be_new_record
     subject.security_answer = nil
     subject.valid?
-    subject.errors[:security_answer].should include("can't be blank")
+    expect(subject.errors[:security_answer]).to include("can't be blank")
   end
 
   it 'should require security answer confirmation if security answer given ' do
     subject.security_answer = 'hello there'
     subject.valid?
-    subject.errors[:security_answer_confirmation].should include "can't be blank"
+    expect(subject.errors[:security_answer_confirmation]).to include("can't be blank")
   end
 
   it 'should not require security answer confirmation if security answer is not given' do
     subject.security_answer = ''
     subject.valid?
-    subject.errors[:security_answer_confirmation].should be_blank
+    expect(subject.errors[:security_answer_confirmation]).to be_blank
   end
 
   it 'should require security answer digest on create' do
     subject = FactoryGirl.build :test_model_with_attribute
-    subject.should be_new_record
+    expect(subject).to be_new_record
 
     # change the security_answer_digest to verify the test
     subject.security_answer_digest = ''
-    lambda do
+    expect do
       begin
         subject.save!
       rescue Exception => ex
-        ex.message.should include("security_answer_digest missing on new record")
+        expect(ex.message).to include("security_answer_digest missing on new record")
         raise
       end
-    end.should raise_error RuntimeError
+    end.to raise_error RuntimeError
   end
 
   it 'should not require security answer digest on update' do
     subject = FactoryGirl.build :test_model_with_attribute
-    subject.should be_new_record
+    expect(subject).to be_new_record
 
     subject.save!
 
@@ -68,72 +68,72 @@ describe TestModelWithAttribute do
     subject.save!
 
     subject.reload
-    subject.security_answer_digest.should be_blank
+    expect(subject.security_answer_digest).to be_blank
   end
 
   it 'should allow to call security answer digest directly if protect setter for digest is not given as option' do
-    lambda do
+    expect do
       subject.security_answer_digest = 'hello'
-    end.should_not raise_error
+    end.not_to raise_error
   end
 
   describe "#security_answer=" do
     it 'should set the security answer and save it encrypted' do
       tmwa = FactoryGirl.create :test_model_with_attribute, security_answer: 'old answer', security_answer_confirmation: 'old answer'
-      tmwa.security_answer_digest.should_not be_blank
+      expect(tmwa.security_answer_digest).not_to be_blank
       old_security_answer_digest = tmwa.security_answer_digest
 
       tmwa.security_answer = 'new answer'
       tmwa.security_answer_confirmation = 'new answer'
-      tmwa.instance_variable_get(:@security_answer).should == 'new answer'
+      expect(tmwa.instance_variable_get(:@security_answer)).to eq('new answer')
       tmwa.save!
-      tmwa.security_answer_digest.should_not be_blank
-      tmwa.security_answer_digest.should_not == old_security_answer_digest
+      expect(tmwa.security_answer_digest).not_to be_blank
+      expect(tmwa.security_answer_digest).not_to eq(old_security_answer_digest)
     end
   end
 
   describe '#authenticate_security_answer' do
     it 'should return subject if security answer given matches the one stored' do
       tmwa = FactoryGirl.create :test_model_with_attribute, security_answer: 'some answer', security_answer_confirmation: 'some answer'
-      tmwa.authenticate_security_answer('some answer').should eq tmwa
+      expect(tmwa.authenticate_security_answer('some answer')).to eq(tmwa)
     end
 
     it 'should return false if security answer given does not match the one stored' do
       tmwa = FactoryGirl.create :test_model_with_attribute, security_answer: 'some answer', security_answer_confirmation: 'some answer'
-      tmwa.authenticate_security_answer('some other answer').should be false
+      expect(tmwa.authenticate_security_answer('some other answer')).to eq(false)
     end
   end
 end
 
 describe TestModelWithAttributeNoValidation do
-  it { should respond_to(:security_answer)               }
-  it { should respond_to(:security_answer=)              }
-  it { should_not respond_to(:security_answer_confirmation)  }
-  it { should_not respond_to(:security_answer_confirmation=) }
-  it { should respond_to(:authenticate_security_answer)  }
+  it { expect(subject).to respond_to(:security_answer)               }
+  it { expect(subject).to respond_to(:security_answer=)              }
+  it { expect(subject).to_not respond_to(:security_answer_confirmation)  }
+  it { expect(subject).to_not respond_to(:security_answer_confirmation=) }
+  it { expect(subject).to respond_to(:authenticate_security_answer)  }
 
   it 'should not require security answer on create' do
-    subject.should be_new_record
+    expect(subject).to be_new_record
     subject.security_answer = nil
     subject.valid?
-    subject.errors[:security_answer].should be_blank
+    expect(subject.errors[:security_answer]).to be_blank
   end
 
   it 'should not require security answer confirmation if security answer given ' do
     subject.security_answer = 'hello there'
     subject.valid?
-    subject.errors[:security_answer_confirmation].should be_blank
+    expect(subject.errors[:security_answer_confirmation]).to be_blank
   end
 
   it 'should not require security answer confirmation if security answer is not given' do
     subject.security_answer = ''
     subject.valid?
-    subject.errors[:security_answer_confirmation].should be_blank
+    expect(subject.errors[:security_answer_confirmation]).to be_blank
   end
 
   it 'should not require security answer digest on create' do
     subject = FactoryGirl.build :test_model_with_attribute_no_validation
-    subject.should be_new_record
+    expect(subject).to be_new_record
 
     # change the security_answer_digest to verify the test
     subject.security_answer_digest = ''
@@ -142,7 +142,7 @@ describe TestModelWithAttributeNoValidation do
 
   it 'should not require security answer digest on update' do
     subject = FactoryGirl.build :test_model_with_attribute_no_validation
-    subject.should be_new_record
+    expect(subject).to be_new_record
 
     subject.save!
 
@@ -152,15 +152,15 @@ describe TestModelWithAttributeNoValidation do
     subject.save!
 
     subject.reload
-    subject.security_answer_digest.should be_blank
+    expect(subject.security_answer_digest).to be_blank
   end
 end
 
 describe TestModelWithAttributeProtectSetterForDigest do
   it 'should not allow to call security answer digest directly' do
-    lambda do
+    expect do
       subject.security_answer_digest = 'hello'
-    end.should raise_error NoMethodError
+    end.to raise_error NoMethodError
   end
 end
 
@@ -168,33 +168,33 @@ describe TestModelWithAttributeWithCaseSensitive do
   it 'should authenticate even if answer is of different case' do
     t = FactoryGirl.create :test_model_with_attribute_with_case_sensitive, security_answer: 'Answer', security_answer_confirmation: 'Answer'
 
-    t.authenticate_security_answer('answer').should eq t
+    expect(t.authenticate_security_answer('answer')).to eq t
   end
 end
 
 describe TestModelWithAttributeDisableConfirmation do
-  it { should_not respond_to(:security_answer_confirmation)  }
-  it { should_not respond_to(:security_answer_confirmation=) }
+  it { expect(subject).to_not respond_to(:security_answer_confirmation)  }
+  it { expect(subject).to_not respond_to(:security_answer_confirmation=) }
   it 'should allow to create and save without any confirmation on security answer' do
     t = FactoryGirl.create :test_model_with_attribute_disable_confirmation, security_answer: 'Answer'
     t.save!
-    t.authenticate_security_answer('another answer').should be false
-    t.authenticate_security_answer('Answer').should eq t
+    expect(t.authenticate_security_answer('another answer')).to eq(false)
+    expect(t.authenticate_security_answer('Answer')).to eq(t)
   end
 end
 
 describe TestModelWithAlternativeSyntax do
-  it { should respond_to(:username)               }
-  it { should respond_to(:username=)              }
-  it { should respond_to(:username_confirmation)  }
-  it { should respond_to(:username_confirmation=) }
-  it { should respond_to(:authenticate_username)  }
+  it { expect(subject).to respond_to(:username)               }
+  it { expect(subject).to respond_to(:username=)              }
+  it { expect(subject).to respond_to(:username_confirmation)  }
+  it { expect(subject).to respond_to(:username_confirmation=) }
+  it { expect(subject).to respond_to(:authenticate_username)  }
 
   context "with no validation" do
-    it { should respond_to(:security_answer)               }
-    it { should respond_to(:security_answer=)              }
-    it { should_not respond_to(:security_answer_confirmation)  }
-    it { should_not respond_to(:security_answer_confirmation=) }
-    it { should respond_to(:authenticate_security_answer)  }
+    it { expect(subject).to respond_to(:security_answer)               }
+    it { expect(subject).to respond_to(:security_answer=)              }
+    it { expect(subject).to_not respond_to(:security_answer_confirmation)  }
+    it { expect(subject).to_not respond_to(:security_answer_confirmation=) }
+    it { expect(subject).to respond_to(:authenticate_security_answer)  }
   end
 end
